8327b9f797e4b32bf233a3bd1fefc93382c53039
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-10-13 04:59:14 -0400

    Adding styles to CoffeeCrud

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/coffees', require('./controllers/coffees'))
 
 app.get('/', (req, res) => {
@@ -19,3 +24,5 @@ app.get('*', (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`)
 });
+
+
--- backend/views/home.jsx
+++ backend/views/home.jsx
@@ -6,9 +6,13 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <a href="/coffees">
+                    <button className="btn-primary">Coffee Page</button>
+                </a>
             </main>
         </Def>
     )
 }
 
 module.exports = home
+
