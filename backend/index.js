6bbe07a1de4719dd86954a4d10a93cd411ce7967
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-10-13 04:28:07 -0400

    Adding home page view

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,14 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/coffees', require('./controllers/coffees'))
 
 app.get('/', (req, res) => {
-    res.render('Home')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
-app.listen(process.env.PORT)
-
-
+// Adding a fallback for PORT if not defined in .env
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${process.env.PORT || 3000}`)
+});
