483c0e9c0036993a45225dd57ad83088b48ea495
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-10-20 09:45:19 -0400

    adding validations

--- backend/views/places/show.jsx
+++ backend/views/places/show.jsx
@@ -7,14 +7,23 @@ function show(data) {
             <main>
                 <h1>{data.place.name}</h1>
                 <img src={data.place.pic} alt={data.place.name} />
-                <p>Located in {data.place.city}, {data.place.state}</p>
+                <h3>
+                    Located in {data.place.city}, {data.place.state}
+                </h3>
                 <p>{data.place.types}</p>
-                
+                <h2>
+                    Description
+                </h2>
+                <h3>
+                    {data.place.showEstablished()}
+                </h3>
+                <h4>
+                    Serving {data.place.cuisines}
+                </h4>
                 {/* Edit button */}
                 <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>
-
                 {/* Delete form */}
                 <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
                     <button type="submit" className="btn btn-danger">
