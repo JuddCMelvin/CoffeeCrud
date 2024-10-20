9219ae8a31c7386d29a2a7565b5aa152b25703d2
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-10-20 09:26:23 -0400

    added seeders

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/places', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
-console.log('Mongo URI:','mongodb://localhost:27017/places');
-
 module.exports.Place = require('./places')
+
+
